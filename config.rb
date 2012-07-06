21ae3c21bc3049a45e66cae7321261b73eefea58
Trevor Davis <davist11@gmail.com>
2012-07-06 11:39:31 -0400

    update a bunch of stuff to be more in line with how i do stuff now

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
-require 'ruby-growl'
+#require 'ruby-growl'
 
 #paths 
 http_path = "/"
@@ -13,22 +13,22 @@ output_style = :compressed
 relative_assets = true
 line_comments = true
 
-on_sprite_saved do |filename|
-
-  unless filename.match('transparent')
-    png8filename = filename.sub(/\.png/, '-fs8.png')
-    %x{compile/pngquant/pngquant #{filename}}
-    %x{mv -f #{png8filename} #{filename}}
-  end
-  
-  puts filename
-  
-  %x{open -a ImageOptim.app #{filename}}
-
-  g = Growl.new "localhost", "ruby-growl",
-    ["ruby-growl Notification"]
-  g.notify "ruby-growl Notification", "Compass sprite generation",
-    "Your sprite is done:" + filename 
-
-end
+# on_sprite_saved do |filename|
+# 
+#   unless filename.match('transparent')
+#     png8filename = filename.sub(/\.png/, '-fs8.png')
+#     %x{compile/pngquant/pngquant #{filename}}
+#     %x{mv -f #{png8filename} #{filename}}
+#   end
+#   
+#   puts filename
+#   
+#   %x{open -a ImageOptim.app #{filename}}
+# 
+#   g = Growl.new "localhost", "ruby-growl",
+#     ["ruby-growl Notification"]
+#   g.notify "ruby-growl Notification", "Compass sprite generation",
+#     "Your sprite is done:" + filename 
+# 
+# end
 
