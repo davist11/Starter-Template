de4361b2f0de1cd1cbe4d77dc56ad6978bf9a30d
Trevor Davis <davist11@gmail.com>
2013-05-07 17:22:42 -0400

    updates to jquery and stuff

--- scripts/global.js
+++ scripts/global.js
@@ -1,7 +1,12 @@
 window.SITE || (SITE = {});
 
-SITE.init = function() {
-	console.log('hello')
-};
+(function($, window, document, undefined) {
+	'use strict';
+
+	SITE.init = function() {
+		console.log('hello')
+	};
+	
+})(jQuery, window, document);
 
 $(document).ready(SITE.init);
